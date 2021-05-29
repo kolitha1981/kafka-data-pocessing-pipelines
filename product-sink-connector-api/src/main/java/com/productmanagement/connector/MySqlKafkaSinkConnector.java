f214185f408bcea251a50c4f326eca38edb88e41
Koitha Perera <kolitha_perera@persistent.com>
2021-05-29 14:36:33 +0530

    Added the record converter for connector.

--- product-sink-connector-api/src/main/java/com/productmanagement/connector/MySqlKafkaSinkConnector.java
+++ product-sink-connector-api/src/main/java/com/productmanagement/connector/MySqlKafkaSinkConnector.java
@@ -1,5 +1,7 @@
 package com.productmanagement.connector;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.ResourceBundle;
@@ -40,22 +42,30 @@ public class MySqlKafkaSinkConnector extends SinkConnector {
 
 	@Override
 	public Class<? extends Task> taskClass() {
-		return null;
+		return MySqlKafkaSinkTask.class;
 	}
 
 	@Override
 	public List<Map<String, String>> taskConfigs(int maxTasks) {
-		return null;
+		LOGGER.info("@@@@@@@@@@ Task configs @@@@@@@@@@@@@@@@@@@@@@@");
+		final List<Map<String, String>> taskConfigurations = new ArrayList<>();
+		final Map<String, String> taskProps = new HashMap<>();
+		taskProps.putAll(this.connectorConfigProperties);
+		for (int i = 0; i < maxTasks; i++) {
+			taskConfigurations.add(taskProps);
+		}
+		return taskConfigurations;
 	}
 
 	@Override
 	public void stop() {
-		
+		LOGGER.info("@@@@@@@@@@ Stopping the connector @@@@@@@@@@@@@@@@@@@@@@@");
 	}
 
 	@Override
 	public ConfigDef config() {
-		return null;
+		LOGGER.info("@@@@@@@@@@ Calling config() of the connector @@@@@@@@@@@@@@@@@@@@@@@");
+		return MysqlKafkaConnectorConfig.configurationdefinition();
 	}
 
 }
