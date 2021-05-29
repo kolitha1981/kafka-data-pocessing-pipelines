13e2f5f14a45eef54c47b0c8300964d73dabf45a
Koitha Perera <kolitha_perera@persistent.com>
2021-05-29 21:05:41 +0530

    Added the debugging functionality for the connector.

---
+++ product-sink-connector-api/src/main/java/ProductConnectorDebugConfig.java
@@ -0,0 +1,13 @@
+import org.apache.kafka.connect.cli.ConnectStandalone;
+
+public class ProductConnectorDebugConfig {
+
+	public static void main(String[] args) {
+		/*
+		ConnectStandalone.main(new String[] {
+				"D://Repositories//personal//kafka-data-pocessing-pipelines//connector-debug-properties//connect-avro-standalone.properties",
+				"D://Repositories//personal//kafka-data-pocessing-pipelines//connector-debug-properties//kafka-mysql-sink.properties" });
+				*/
+	}
+
+}
